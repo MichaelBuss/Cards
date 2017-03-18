f7d5dfc32e2af65a269cda2dd0cbe6f3bc8c93fb
Michael Buss Andersen <michaelbussandersen@gmail.com>
2017-03-18 23:39:11 +0100

    Tilføjet python shell eksempel

--- Cards/ViewController.swift
+++ Cards/ViewController.swift
@@ -11,7 +11,6 @@ import Cocoa
 class ViewController: NSViewController {
 
     override func viewDidLoad() {
-        super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
---
+++ Cards/WindowController.swift
@@ -0,0 +1,36 @@
+//
+//  WindowController.swift
+//  Cards
+//
+//  Created by Michael Buss Andersen on 18/03/2017.
+//  Copyright © 2017 NoobLabs. All rights reserved.
+//
+
+import Cocoa
+
+class WindowController: NSWindowController {
+    @IBOutlet weak var runOutlet: NSButton!
+
+    
+    override func windowDidLoad() {
+        super.windowDidLoad()
+    
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+    
+    @IBAction func runAction(_ sender: Any) {
+        //When the run button is pressed
+        runOutlet.isEnabled = false //Disable button while code is running
+        
+        let path = "/usr/bin/env" //( Path
+        let arguments = [Bundle.main.path(forResource: "Hello", ofType: "py")]
+        
+        let task = Process.launchedProcess(launchPath: path, arguments: arguments as! [String])
+        task.waitUntilExit()
+        
+        runOutlet.isEnabled = true //Enable button again
+    }
+
+
+
+}
